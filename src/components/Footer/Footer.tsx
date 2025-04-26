35304654fdc115b18f45e56d59d1777d681b5896
Gabriel Rodrigues <apenasgabs.dev@gmail.com>
2025-04-26 00:21:29 -0300

    feat: :sparkles: add Footer component with styling and integrate it into the App

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <section id="credit">
+        <p>
+          Made with ❤️ by <a href="https://github.com/Apenasgabs">Apenasgabs</a>
+        </p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "@components/Footer/Footer";
 import React from "react";
 import TreeView from "../components/TreeView";
 
@@ -5,11 +6,12 @@ const App: React.FC = () => {
   const sampleHtml = `<div class="container"><h1>Hello World</h1><p>This is a sample HTML structure.</p></div>`;
 
   return (
-    <div>
+    <div style={{ paddingBottom: "60px" }}>
       <h1>
         <code>{`<TreeView />`}</code> 🌳
       </h1>
       <TreeView html={sampleHtml} />
+      <Footer />
     </div>
   );
 };
